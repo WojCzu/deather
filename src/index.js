618711998848bdbd37eea6ef08ce1e0b7b60c4e3
WojCzu <wojciech.czuma@gmail.com>
2021-07-14 14:46:30 +0200

    Revert "install GSAP"
    This reverts commit f18af2b1a7eb143f20216cd9fca03b15e70cac14.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import "./sass/style.scss";
 import Utils from "./code/utils";
-import { gsap } from "gsap";
-import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
-
-// gsap.registerPlugin(MorphSVGPlugin);
 
 const utils = new Utils();
 utils.startDarkMode();
