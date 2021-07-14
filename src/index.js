f18af2b1a7eb143f20216cd9fca03b15e70cac14
WojCzu <wojciech.czuma@gmail.com>
2021-07-14 14:42:46 +0200

    install GSAP

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import "./sass/style.scss";
 import Utils from "./code/utils";
+import { gsap } from "gsap";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+
+// gsap.registerPlugin(MorphSVGPlugin);
 
 const utils = new Utils();
 utils.startDarkMode();
